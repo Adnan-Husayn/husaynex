0e3ef5c75673037ae0c626ab17f7a218606431df
Adnan-Husayn <iamsaiyedzayn@gmail.com>
2025-09-30 11:43:57 +0530

    added block struct & hashing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello from husaynex");
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Block {
+    pub index: u64,
+    pub timestamp: DateTime<Utc>,
+    pub data: String,
+    pub nonce: u64,
+    pub prev_hash: String,
+    pub hash: String,
 }
+
+impl Block {
+    pub fn new(index: u64, data: String, prev_hash: String) -> Self {
+        Self {
+            index,
+            timestamp: Utc::now(),
+            data,
+            nonce: 0,
+            prev_hash,
+            hash: String::new(),
+        }
+    }
+
+    pub  fn compute_hash(&self) -> String {
+        let payload = format!(
+            "{}{}{}{}{}",
+            self.index,
+            self.timestamp.to_rfc3339(),
+            self.data,
+            self.nonce,
+            self.prev_hash
+        );
+
+        blake3::hash(payload.as_bytes()).to_hex().to_string()
+    }
+}
+
+fn main() {}
